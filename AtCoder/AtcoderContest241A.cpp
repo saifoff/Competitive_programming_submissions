fa4a3fd477a213b29c1bf8e00b74473600b93dc0
saifoff <saifulttnn@gmail.com>
2022-02-26 19:35:12 +0600

    Adding Atcoder contest241 code Submissions

---
+++ AtCoder/AtcoderContest241A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n[10];
+    for(int i=0;i<10;i++){
+        cin>>n[i];
+    }
+    int f;
+    cout<<n[n[n[0]]];
+}
---
+++ AtCoder/AtcoderContest241B.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int binarySearch(long long int array[], long long int x, int low, int high) {
+  
+	// Repeat until the pointers low and high meet each other
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x){
+        array[mid]=-2;
+        return mid;
+    }
+
+    if (array[mid] < x)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1;
+}
+
+int main()
+{
+    int n,m,f;
+    cin>>n>>m;
+    long long int a[n],b[m];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<m;i++){
+        cin>>b[i];
+    }
+    if(n<m){
+        cout<<"No"<<endl;
+    }
+    else{
+        for(int i=0;i<m;i++){
+            f=NULL;
+            f=binarySearch(a,b[i],0,n-1);
+            if(f==-1){
+                cout<<"No"<<endl;
+                return 0;
+            }
+        }
+        cout<<"Yes"<<endl;
+
+    }
+}
